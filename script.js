9b3067f10affc58189666671177461b9c5032665
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-09 18:43:39 +0800

    Add Youtube link button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function openTab() {
+function openIGB() {
     window.open("https://www.instagram.com/blxvk.out/", "_blank");
+};
+
+function openHocc() {
+    window.open("https://www.youtube.com/watch?v=lqKMCNyrpzg?start=3244", "_blank");
 };
